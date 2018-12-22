c16c5a9301ee370e5fc8b63ea4af8a9a0044c9a5
theghast2022 <44522450+theghast2022@users.noreply.github.com>
2018-12-23 00:34:12 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -41,7 +41,7 @@ client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== "388357326614167563") return;
+  if (message.author.id !== "524233109525364776") return;
 
   
   if (message.content.startsWith(prefix + 'wt')) {
