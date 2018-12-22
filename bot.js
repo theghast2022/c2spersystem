ba7c2c00d61410e332496c3f9c9a6fa44d967324
theghast2022 <44522450+theghast2022@users.noreply.github.com>
2018-12-22 23:42:56 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -758,7 +758,7 @@ client.on('message', async message => {
   }
 });
 client.on('guildMemberAdd', (member) => {
-member.addRole(member.guild.roles.find('name', 'Player'));  
+member.addRole(member.guild.roles.find('name', '.ET -'));  
 });
 
 
